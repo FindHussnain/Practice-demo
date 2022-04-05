82df12c26ebb81947aeced9f7180da9dffd79d5e
Hussnain Younas <FindHussnain@gmail.com>
2022-04-05 12:02:29 +0500

    used GraphQL with rails

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    field :items, 
+    [Types::ItemType],
+    null: false,
+    description: "Return a list of items"
+
+    def items
+      Item.all
+    end
+  end
+end
